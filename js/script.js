21e9b682e6a169b00262c84cd8ff0b70345f26ec
fabiokleis <fabiohkrc@gmail.com>
2021-03-12 23:19:44 -0300

    lang

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// mobile nav bar
+function nav_mobile(){
+    let link = document.getElementById("link");
+    if(link.style.display = "inline"){
+        link.style.display = "none";
+    }else {
+        link.style.display = "inline";
+    }
+}
