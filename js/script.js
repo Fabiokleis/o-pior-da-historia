653282ccac843e50c9b5a233fbba830273672dcc
fabiokleis <fabiohkrc@gmail.com>
2021-03-15 22:23:56 -0300

    boa parte feita do mobile, e novas funcionalidades adicionadas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-var img_array = ["./img/3ep.png","./img/4ep.png","./img/5ep.png","./img/9ep.png","./img/14ep.png","./img/16ep.png","./img/17ep.png"];
+var img_array = ["./thumbs/1ep.jpg","./thumbs/2ep.jpg","./thumbs/3ep.jpg","./thumbs/4ep.jpg","./thumbs/5ep.jpg","./thumbs/6ep.jpg","./thumbs/7ep.jpg","./thumbs/8ep.jpg","./thumbs/9ep.jpg","./thumbs/10ep.jpg","./thumbs/11ep.jpg","./thumbs/12ep.jpg","./thumbs/13ep.jpg","./thumbs/14ep.jpg","./thumbs/15ep.jpg","./thumbs/16ep.jpg","./thumbs/17ep.jpg","./thumbs/18ep.jpg","./thumbs/19ep.jpg"];
+var track_array = ["./tracks/1ep.mp3","./tracks/2ep.mp3","./tracks/3ep.mp3","./tracks/4ep.mp3","./tracks/5ep.mp3","./tracks/6ep.mp3", "./tracks/7ep.mp3","./tracks/8ep.mp3","./tracks/9ep.mp3","./tracks/10ep.mp3","./tracks/11ep.mp3","./tracks/12ep.mp3","./tracks/13ep.mp3","./tracks/14ep.mp3","./tracks/15ep.mp3","./tracks/16ep.mp3","./tracks/17ep.mp3","./tracks/18ep.mp3","./tracks/19ep.mp3"];
 
+let audio = document.getElementById("first");
+let timer = document.getElementById("current-time");
+let slider = document.getElementById("seek-slider");
+let max_time = document.getElementById("max_time");
 
 // mobile nav bar
 function nav_mobile(flp){
@@ -37,10 +42,12 @@ function pause_audio(){
     let audio_ = document.getElementById("first");
 
     if(audio_.paused == true){
-        player.setAttribute("src", "./img/pause.png");
+        player.setAttribute("src", "./control/pause.png");
+        requestAnimationFrame(whilePlaying);
         audio_.play();
     }else{
-        player.setAttribute("src", "./img/play.png");
+        player.setAttribute("src", "./control/play.png");
+        cancelAnimationFrame(rAF);
         audio_.pause();
     }
 }
@@ -50,73 +57,50 @@ function mute_audio(){
     let ip = document.getElementById("volume-control");
     if(audio_.muted == false){
     
-        vol.setAttribute("src", "./img/mutado.png");
+        vol.setAttribute("src", "./control/mutado.png");
         audio_.muted = true;
-        vol.style.width ="53px";
-        vol.style.height ="30px"
         ip.value = 0;
         
     }else{
-        vol.setAttribute("src", "./img/som.png");
-        vol.style.width ="28px";
-        vol.style.height ="28px"
+        vol.setAttribute("src", "./control/som.png");
+        
         ip.value = 50;
         audio_.muted = false;
     }
 }
 
-function remove_slider(){
-    let vol = document.getElementById("volume-control");
-    vol.style.display = "none";
-    slider_.textContent = "";
-}
+
 function thisVolume(volume_value) { 
     let vol = document.getElementById("vol");
     let audio_ = document.getElementById("first");
     document.getElementById("volume-control").innerHTML=volume_value; 
-    console.log(volume_value);
+
     audio_.volume = volume_value / 100;
     if(audio_.volume == 0){
-        vol.setAttribute("src", "./img/mutado.png");
-        vol.style.width ="53px";
-        vol.style.height ="30px"
+        vol.setAttribute("src", "./control/mutado.png");
+        
     }else{
-        vol.setAttribute("src", "./img/som.png");
-        vol.style.width ="28px";
-        vol.style.height ="28px"
+        vol.setAttribute("src", "./control/som.png");
+        
     }
 }
 
 function prox_img(ola){
     let curr = document.getElementById("current_track");
-    let card_curr = document.getElementsByClassName("card");
+    let card_curr = document.getElementById("playlist");
     let imag = document.getElementById("home");    
     let num = parseInt(ola.getAttribute("data-num"));
-    console.log(card_curr[num]);
-    curr.innerHTML = card_curr[num];
+    let audio_ = document.getElementById("first");
+    let player = document.getElementById("player");
+    curr.children[0].innerHTML = card_curr.children[num].innerHTML;
+    player.setAttribute("src", "./control/play.png");
     
-    console.log(num);
+    audio_.setAttribute("src", track_array[num]);
     imag.setAttribute("src",img_array[num]);    
-    
-}
-
-function ant_img(){
-    let imag = document.getElementById("home");    
-    let ola = document.getElementById("prox");
-    let a = imag.getAttribute("src");
-    console.log(img_array[0]);
-    console.log(a);
-    if(a != img_array[0]){
-        let pp = parseInt(ola.dataset.num);
-        pp--;
-        ola.dataset.num = pp;
-        
-        imag.setAttribute("src",img_array[parseInt(ola.dataset.num)]);       
-    }
 }
 
 let playlist = document.getElementById("playlist");
-for(k = 0; k <= img_array.length; k++){
+for(k = 0; k < img_array.length; k++){
     playlist.innerHTML += "<div class='card' onclick='prox_img(this)'></div>";
     playlist.children[k].setAttribute("data-num", k);
     playlist.children[k].innerHTML += "<img class='icon'><p class='text'>ola mundo</p>";
@@ -125,3 +109,56 @@ let j;
 for(j = 0; j < img_array.length; j++){
     console.log(playlist.children[j].firstChild.setAttribute("src",img_array[j]));
 }
+
+
+// audio input sync
+
+audio.addEventListener("loadedmetadata", () => {
+
+    max_time.innerText = calculate_time(audio.duration);
+    setSliderMax();
+
+});
+
+function calculate_time(sec){
+    let minut = Math.floor(sec/60);
+    let second = Math.floor(sec%60);
+    let returnedSecs = second < 10 ? `0${second}` : `${second}`
+    return `${minut}:${returnedSecs}`
+}
+function setSliderMax(){
+    slider.max = Math.floor(audio.duration);
+}
+function showRangeProgress(rangeInput){
+    if(rangeInput == slider){
+        timer.style.setProperty('--seek-before-width', rangeInput.value / range.rangeInput.max *100 +"%")
+    }
+}
+
+slider.addEventListener('input', () => {
+    timer.innerText = calculate_time(slider.value);
+    
+    if(!audio.paused){
+        cancelAnimationFrame(rAF);
+    }
+});
+
+
+slider.addEventListener('change', () => {
+    audio.currentTime = slider.value;
+    if(!audio.paused){
+        requestAnimationFrame(whilePlaying)
+    }
+});
+
+slider.addEventListener('timeupdate',() => {
+    slider.value = Math.floor(audio.currentTime);
+});
+let rAF = null;
+const whilePlaying = () => {
+    slider.value = Math.floor(audio.currentTime);
+    timer.innerText = calculate_time(slider.value);
+    timer.style.setProperty('--seek-before-width', `${slider.value/slider.max*100}%`);
+    rAF = requestAnimationFrame(whilePlaying);
+}
+
